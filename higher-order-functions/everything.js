b284d34954e6f71ee6e7f13e84251d41d7eab29b
bitMan <mrterm404@gmail.com>
2024-08-05 13:29:27 +0530

    refactor minor

--- higher-order-functions/everything.js
+++ higher-order-functions/everything.js
@@ -4,23 +4,22 @@ function every(array, test)
 
     for (let i = 0; i < array.length; i++)
     {
-    //    console.log(test(array[i]))
+    
         if (test(array[i]))
         {
             trueCount.push(array[i])
         } 
     }
 
-    // console.log(trueCount)
 
     if (trueCount.length === array.length)
     {
-        return true
+        return true 
     } else {
         return false
     }
 }
 
-console.log(every([1, 3, 5], n => n < 10));
+console.log(every([1, 3, 5], n => n < 10))
 
 console.log(every([2, 4, 16], n => n < 10))
